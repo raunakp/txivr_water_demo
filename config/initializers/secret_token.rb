163ea8bf8c631af1001f4db4ce167e838b6aa6f3
Raunak Poddar <raunak.poddar@gmail.com>
2012-09-12 16:50:12 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TxivrWaterDemo::Application.config.secret_token = '43f838dca2569f30df35dd087f8efeba252f67622537a1086187a486e18da1a546281161862333a7e857cb91a9c3b4316aaa051a4613f286ee509f958a9cf949'
