01809e3b95c2cdf355e77e05c8f16e2ea7cac5a9
Raunak Poddar <raunak.poddar@gmail.com>
2012-09-18 17:13:07 +0530

    validate email, account number

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: readings
+#
+#  id            :integer          not null, primary key
+#  meter_reading :string(255)
+#  user_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Reading < ActiveRecord::Base
   attr_accessible :meter_reading, :user_id
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id             :integer          not null, primary key
+#  phone_number   :string(255)
+#  fname          :string(255)
+#  lname          :string(255)
+#  email          :string(255)
+#  address        :string(255)
+#  account_number :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class User < ActiveRecord::Base
   attr_accessible :account_number, :address, :email, :fname, :lname, :phone_number
   has_many :readings
+
+  before_save {|user| user.email = email.downcase}
+
+  validates :fname, presence: true
+  validates :account_number, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates_length_of :account_number, :is => 8, :message => "should be of 8 digits"
+  validates_numericality_of :account_number, :only_integer => true
+
 end
