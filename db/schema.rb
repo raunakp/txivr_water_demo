08d87ca7f2ee4cc5e57e645317045f7a57247d37
Raunak Poddar <raunak.poddar@gmail.com>
2012-09-21 19:05:27 +0530

    add bootstrap gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918110820) do
+ActiveRecord::Schema.define(:version => 20120912151225) do
 
   create_table "readings", :force => true do |t|
     t.string   "meter_reading"
@@ -30,7 +30,5 @@ ActiveRecord::Schema.define(:version => 20120918110820) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
